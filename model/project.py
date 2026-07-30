c21935d6ad22f2ea310ece7bb71fb0d34e1f8d33
elnir-elnir <elnir@ya.ru>
2026-07-31 05:23:37 +0700

    Домашнее задание 25 Структура объекта Project дополнена (справочно) информацией о полях в БД для таблицы mantis_project_table

--- model/project.py
+++ model/project.py
@@ -6,9 +6,13 @@
 
 class Project:
     def __init__(self, id=None, name=None, status=None, inherit_global=None, view_state=None, description=None):
-        self.id = id
-        self.name = name
-        self.status = status
-        self.inherit_global = inherit_global
-        self.view_state = view_state
-        self.description = description
+        self.id = id # Primary
+        self.name = name # обязательное поле (UNI)
+        self.status = status # Optional
+        #enabled # появляется после создания, Optional
+        #access_min # из БД, Optional
+        #file_path # из БД, Optional
+        self.inherit_global = inherit_global # Optional
+        self.view_state = view_state # Optional
+        self.description = description # Optional
+        #category_id # из БД, Optional
